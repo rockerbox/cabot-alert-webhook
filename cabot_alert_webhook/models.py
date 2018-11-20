534c891cb546431c247c7fa7ee49a251f83b4da2
Sameen Karim <sameenkarim@gmail.com>
2018-11-20 15:39:13 -0500

    remove old logger

--- cabot_alert_webhook/models.py
+++ cabot_alert_webhook/models.py
@@ -26,7 +26,6 @@ class WebhookAlert(AlertPlugin):
         webhook_urls = [u.webhook_url for u in WebhookAlertUserData.objects.filter(user__user__in=users)]
 
         for url in webhook_urls:
-            logger.info(message)
             requests.post(
                 url,
                 data=json.dumps(payload)
