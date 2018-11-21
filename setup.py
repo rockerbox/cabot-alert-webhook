463ae4ce640952548078cae9a97fa6d852d7159d
Sameen Karim <sameenkarim@gmail.com>
2018-11-21 13:50:03 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import os
+import sys
 
-setup(name='cabot-alert-webhook',
-      version='0.0.1',
-      description='A Webhook plugin for Cabot',
-      author='Rockerbox',
-      author_email='engineering@rockerbox.com',
-      url='https://github.com/rockerbox/cabot-alert-webhook',
-      packages=find_packages(),
-      download_url = 'https://github.com/rockerbox/cabot-alert-webhook/tarball/master',
-      bugtrack_url = "https://github.com/rockerbox/cabot-alert-webhook/issues",
-      keywords = ['cabot', 'webhook', 'status check'],
-     )
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+if sys.version_info[0] < 3:
+    with open(os.path.join(BASE_DIR, 'README.rst')) as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='cabot-alert-webhook',
+    version='0.1.0',
+    description='A Webhook plugin for Cabot',
+    long_description=long_description,
+    author='Rockerbox',
+    author_email='engineering@rockerbox.com',
+    url='https://github.com/rockerbox/cabot-alert-webhook',
+    packages=find_packages(),
+    download_url = 'https://github.com/rockerbox/cabot-alert-webhook/tarball/master',
+    bugtrack_url = "https://github.com/rockerbox/cabot-alert-webhook/issues",
+    keywords = ['cabot', 'webhook', 'status check'],
+)
